346cfbb9d87cd11595ed14e77aa5d2b8c4f3485d
esBeee <sebastianbitzer@posteo.de>
2016-04-21 05:23:15 +0200

    Clean up

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,15 +1,18 @@
 # The Statement class describes an interrogation someone is interested in, like
-# "What politician X has done was correct!". Now other users are able to add pro and contra
+# "What politician X has done was wrong!". Now other users are able to add pro and contra
 # arguments. The clou: those arguments are an instance of class Statement themselves. So also those
 # arguments can be subject of a debate.
 class Statement < ApplicationRecord
   belongs_to :user
 
   # Has many links to arguments (which are also objects of type Statement).
+  # Is only used inside this class to help define has_many :arguments.
   has_many :argumental_questioning_links
   # Has many links to PRO arguments.
+  # Is only used inside this class to help define has_many :pro_arguments.
   has_many :pro_argumental_questioning_links, -> { where(is_pro_argument: true) }, class_name: "ArgumentalQuestioningLink"
   # Has many links to CONTRA arguments.
+  # Is only used inside this class to help define has_many :contra_arguments.
   has_many :contra_argumental_questioning_links, -> { where(is_pro_argument: false) }, class_name: "ArgumentalQuestioningLink"
   # Has many arguments (also objects of type Statement).
   has_many :arguments, through: :argumental_questioning_links
