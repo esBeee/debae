eb5352c906e7cf271e9458c4f1a156dc2369eceb
esBeee <sebastianbitzer@posteo.de>
2016-04-21 02:45:30 +0200

    Define relations between a questioning and its arguments

--- app/models/questioning.rb
+++ app/models/questioning.rb
@@ -1,9 +1,23 @@
 # The Questioning class describes an interrogation someone is interested in, like
 # "What politician X has done was correct!". Now other users are able to add pro and contra
 # arguments. The clou: those arguments are an instance of class Questioning themselves. So also those
-# arguments can be argued about.
+# arguments can be subject of a debate.
 class Questioning < ApplicationRecord
   belongs_to :user
 
+  # Has many links to arguments (which are also objects of type Questioning).
+  has_many :argumental_questioning_links
+  # Has many links to PRO arguments.
+  has_many :pro_argumental_questioning_links, -> { where(is_pro_argument: true) }, class_name: "ArgumentalQuestioningLink"
+  # Has many links to CONTRA arguments.
+  has_many :contra_argumental_questioning_links, -> { where(is_pro_argument: false) }, class_name: "ArgumentalQuestioningLink"
+  # Has many arguments (also objects of type Questioning).
+  has_many :arguments, through: :argumental_questioning_links
+  # Has many pro arguments.
+  has_many :pro_arguments, through: :pro_argumental_questioning_links, source: :argument
+  # Has many contra arguments.
+  has_many :contra_arguments, through: :contra_argumental_questioning_links, source: :argument
+
+  # Validations
   validates :body, presence: true, length: { in: 2..260 }
 end
--- spec/models/questioning_spec.rb
+++ spec/models/questioning_spec.rb
@@ -53,4 +53,83 @@ RSpec.describe Questioning, type: :model do
       end
     end
   end
+
+  # Test that the utilities for the associated objects are defined.
+  describe "association utilities" do
+    # Make sure there's a getter for the questioning's argumental_questioning_links.
+    describe "#argumental_questioning_links" do
+      let(:argumental_questioning_link) do
+        FactoryGirl.build_stubbed(:argumental_questioning_link, questioning: @questioning)
+      end
+
+      it "returns all associated argumental questioning links" do
+        # Add argumental_questioning_link to the questioning's argumental_questioning_links.
+        # This implicitly tests the existence of a setter.
+        @questioning.argumental_questioning_links << argumental_questioning_link
+
+        # Now test that the getter delivers the argumental_questioning_link.
+        expect(@questioning.argumental_questioning_links).to include argumental_questioning_link
+
+        # Make sure only this one link was delivered.
+        expect(@questioning.argumental_questioning_links.size).to eq 1
+      end
+    end
+
+    # Make sure there's a getter for the questioning's arguments.
+    describe "#arguments" do
+      let(:argument) { FactoryGirl.create(:questioning) }
+      let!(:argumental_questioning_link) do
+        FactoryGirl.create(:argumental_questioning_link, questioning: @questioning, argument: argument)
+      end
+
+      it "returns all associated arguments" do
+        # Add argumental_questioning_link to the questioning's argumental_questioning_links.
+        @questioning.argumental_questioning_links << argumental_questioning_link
+
+        # Now test that the getter delivers the argument.
+        expect(@questioning.arguments).to include argument
+
+        # Make sure only this one argument was delivered.
+        expect(@questioning.arguments.size).to eq 1
+      end
+    end
+
+    # Test that the pro_arguments getter is defined correctly.
+    describe "#pro_arguments" do
+      let!(:pro_argument) do
+        FactoryGirl.create(:argumental_questioning_link, is_pro_argument: true, questioning: @questioning).argument
+      end
+      let!(:contra_argument) do
+        FactoryGirl.create(:argumental_questioning_link, is_pro_argument: false, questioning: @questioning).argument
+      end
+
+      it "returns all pro arguments" do
+        # Test that the scope delivers the pro argument.
+        expect(@questioning.pro_arguments).to include pro_argument
+
+        # Make sure only this one argument was delivered. (Implies that
+        # the contra argument wasn't)
+        expect(@questioning.pro_arguments.size).to eq 1
+      end
+    end
+
+    # Test that the contra_arguments getter is defined correctly.
+    describe "#contra_arguments" do
+      let!(:pro_argument) do
+        FactoryGirl.create(:argumental_questioning_link, is_pro_argument: true, questioning: @questioning).argument
+      end
+      let!(:contra_argument) do
+        FactoryGirl.create(:argumental_questioning_link, is_pro_argument: false, questioning: @questioning).argument
+      end
+
+      it "returns all contra arguments" do
+        # Test that the scope delivers the pro argument.
+        expect(@questioning.contra_arguments).to include contra_argument
+
+        # Make sure only this one argument was delivered. (Implies that
+        # the pro argument wasn't)
+        expect(@questioning.contra_arguments.size).to eq 1
+      end
+    end
+  end
 end
