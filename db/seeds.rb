b50c8e3885fc8b54f2718a8c2d7ea81c773205ae
esBeee <sebastianbitzer@posteo.de>
2016-04-21 19:40:55 +0200

    Include pagiantion links on statements index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+email = "a@b.de"
+user = User.find_by(email: email)
+if user.nil?
+  user = FactoryGirl.create(:user, email: email)
+end
+puts "User #{email} with password #{user.password} now exists."
+
+# Create statements
+# FactoryGirl.create_list(:statement, 45)
+# puts "45 top level statements created."
--- spec/features/statements/index_visitings_spec.rb
+++ spec/features/statements/index_visitings_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "IndexVisitings", type: :feature do
+  let(:statements_per_page) { WillPaginate.per_page }
+
   describe "lists statements" do
-    let(:statements_per_page) { WillPaginate.per_page }
     # It's very important that the statements created here have pairwise distinc
     # values for the body attribute. Otherwise tests in this file will not
     # work as expected.
@@ -71,6 +72,34 @@ RSpec.feature "IndexVisitings", type: :feature do
     it "displays link to statements" do
       statements.each do |statement|
         expect(page).to have_link(statement.body, href: statement_path(statement))
+      end
+    end
+  end
+
+  # Just test that the pagination links are there.
+  # Since the links stem from gem will_paginate, no indepth testing
+  # will be done here.
+  describe "pagination links" do
+    let(:link_to_next_page) { I18n.t("will_paginate.next_label") }
+
+    context "when more than one page of statements is available" do
+      let!(:statements) { FactoryGirl.create_list(:statement, statements_per_page + 1) }
+      let(:path_to_next_page) { "/?page=2" }
+
+      before { visit statements_path }
+
+      it "displays link to the next page" do
+        expect(page).to have_link(link_to_next_page, href: path_to_next_page)
+      end
+    end
+
+    context "when not more than one page of statements is available" do
+      let!(:statements) { FactoryGirl.create_list(:statement, statements_per_page) }
+
+      before { visit statements_path }
+
+      it "doesn't display link to the next page" do
+        expect(page).not_to have_link(link_to_next_page)
       end
     end
   end
