d8db64e3a5b37dbf129223d2c58f271a02167dd0
esBeee <sebastianbitzer@posteo.de>
2016-04-29 00:43:08 +0200

    Also request the profile url from facebook on OAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,7 +243,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
-    scope: 'public_profile,email', info_fields: 'email,name,urls',
+    scope: 'public_profile,email', info_fields: 'email,name,link',
     image_size: { width: 500, height: 500 }, secure_image_url: true
 
   # ==> Warden configuration
