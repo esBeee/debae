6b7f305a05990fbc3299dcbe0f2671d1a92cb306
esBeee <sebastianbitzer@posteo.de>
2016-05-06 17:55:06 +0200

    Adjust tests to recent changes regarding the statement-history

--- spec/features/layouts/back_button_spec.rb
+++ spec/features/layouts/back_button_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "BackButton", type: :feature, statements_helper: true do
-  let(:statement_argument_link) { FactoryGirl.create(:statement_argument_link) }
-  let(:statement_1) { statement_argument_link.statement }
-  let(:statement_2) { statement_argument_link.argument }
+  pending "Now only works with JS and local browser storage."
+  # let(:statement_argument_link) { FactoryGirl.create(:statement_argument_link) }
+  # let(:statement_1) { statement_argument_link.statement }
+  # let(:statement_2) { statement_argument_link.argument }
 
-  let(:back_link_id) { "i.fa-arrow-circle-left" }
+  # let(:back_link_id) { "i.fa-arrow-circle-left" }
 
-  context "when the user has already visited two pages" do
-    before do
-      visit statement_path(statement_1)
+  # context "when the user has already visited two pages" do
+  #   before do
+  #     visit statement_path(statement_1)
 
-      # Since statement_2 is an argument for statement_1, a link
-      # to statement_2 should exist on the page for statement_1.
-      click_link body(statement_2)
-    end
+  #     # Since statement_2 is an argument for statement_1, a link
+  #     # to statement_2 should exist on the page for statement_1.
+  #     click_link body(statement_2)
+  #   end
 
-    it "brings the user back to the previously visited page" do
-      find(back_link_id).click
-      pending "This test doesn't work for any reason"
-      expect(page.current_path).to eq statement_path(statement_1)
-    end
-  end
+  #   it "brings the user back to the previously visited page" do
+  #     find(back_link_id).click
+  #     pending "This test doesn't work for any reason"
+  #     expect(page.current_path).to eq statement_path(statement_1)
+  #   end
+  # end
 end
--- spec/views/statements/show.html.erb_spec.rb
+++ spec/views/statements/show.html.erb_spec.rb
@@ -11,14 +11,6 @@ RSpec.describe "statements/show.html.erb", type: :view, statements_helper: true 
   it "displays the statement's body" do
     expect(rendered).to have_content(body(statement))
   end
-
-  it "displays container for pro arguments" do
-    expect(rendered).to have_css(".arguments-container.pro")
-  end
-
-  it "displays container for contra arguments" do
-    expect(rendered).to have_css(".arguments-container.contra")
-  end
   
   it "displays headline for pro arguments" do
     expect(rendered).to have_content(I18n.t("statements.show.headlines.pro_arguments"))
