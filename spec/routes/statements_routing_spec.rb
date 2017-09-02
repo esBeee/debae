69cf029d195d13f0edb191c8c226100862648344
esBeee <sebastianbitzer@posteo.de>
2017-09-02 20:59:20 +0200

    Clean up

--- spec/routes/statements_routing_spec.rb
+++ spec/routes/statements_routing_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Statement routes", type: :routing do
     expect(get(statements_path)).to route_to("statements#index")
   end
 
-  it "routes GET root_path to the statements controller's index action" do
-    expect(get(root_path)).to route_to("statements#index")
+  it "routes GET statements_path to the statements controller's index action" do
+    expect(get(statements_path)).to route_to("statements#index")
   end
 
   it "routes GET statement_path(:id) to the statements controller's show action" do
