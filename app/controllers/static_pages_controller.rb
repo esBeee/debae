a9814578e483264714c25f1f20bd8702f5627093
esBeee <sebastianbitzer@posteo.de>
2017-09-17 14:53:57 +0200

    Only show top level statements on root page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   # GET /
   def onboarding
     @statements = Statement
+      .where(top_level: true)
       .select("statements.*, COUNT(statement_argument_links.id) statement_argument_links_count")
       .joins("LEFT OUTER JOIN statement_argument_links ON statement_argument_links.statement_id = statements.id")
       .group("statements.id")
